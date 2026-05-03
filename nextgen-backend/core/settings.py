0dc1e94bc97f1f666451ee0247bfa532aa482c87
Neeraj kumar saini <alwaysneerudj@gmail.com>
2026-05-03 15:54:34 +0530

    Update CORS settings to allow all origins
    Changed CORS_ALLOWED_ORIGINS to True for all origins.

--- nextgen-backend/core/settings.py
+++ nextgen-backend/core/settings.py
@@ -91,10 +91,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ALLOWED_ORIGINS = os.getenv(
-    "CORS_ALLOWED_ORIGINS",
-    "http://localhost:3000,http://127.0.0.1:3000"
-).split(",")
+CORS_ALLOWED_ORIGINS = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config("EMAIL_HOST", default="")
